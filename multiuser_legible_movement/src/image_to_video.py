aefc87777687eeb4fbf36468a52c2b08d0bac76e
miguel-faria <miguel.afonso.faria@gmail.com>
2020-01-29 16:27:40 +0000

    Update with videos up to configuration 4

--- multiuser_legible_movement/src/image_to_video.py
+++ multiuser_legible_movement/src/image_to_video.py
@@ -40,9 +40,9 @@ def main():
           
     print('[IMAGE TO VIDEO] Creating video from the images')
     if len(size) > 0:
-        out = cv2.VideoWriter('../data/videos/configuration_' + args.configuration[0] + '/' + args.user[0] + '/' +
+        out = cv2.VideoWriter('../data/videos/configuration_' + args.configuration[0] + '/' + args.user[0] + '/orig/' +
                               args.user[0] + '_movement_' + args.target[0] + '_' + args.movement[0] + '.mp4',
-                              cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 10, size)
+                              cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 14, size)
 
         for i in range(len(img_array)):
             out.write(img_array[i])
